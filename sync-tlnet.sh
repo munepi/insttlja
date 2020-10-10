1802ce0c4acab1c1e505a1cddb1ef0d003b84d9d
Munehiro Yamamoto <munepixyz@gmail.com>
2020-10-10 16:21:56 +0900

    sync-tlnet.sh: supported versioned container

--- sync-tlnet.sh
+++ sync-tlnet.sh
@@ -4,7 +4,7 @@ export LANG=C LC_ALL=C
 DEFAULT_TLNETROOT=${TLNETROOT:-/var/local/tlnet}
 
 mkdir -p ${DEFAULT_TLNETROOT}
-rsync -v -rpt --delete \
+rsync -v -rptl --delete \
     rsync://ftp.jaist.ac.jp/pub/CTAN/systems/texlive/tlnet/ ${DEFAULT_TLNETROOT}
 
 echo $(basename $0): done.
